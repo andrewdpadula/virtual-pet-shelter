285b15fbf5d97eb1a3b8ca520ba0026ac6a1d886
Andrew Padula <andrewdpadula@gmail.com>
2018-06-04 09:39:44 -0400

    Project submission

--- src/test/java/VirtualPetTest.java
+++ src/test/java/VirtualPetTest.java
@@ -1,26 +1,27 @@
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class VirtualPetTest {
-VirtualPet testPet = new VirtualPet("Test Pet Name", "Test Pet Description");
+	VirtualPet testPet = new VirtualPet("Test Pet Name", "Test Pet Description");
 
-@Test
-public void shouldReturnHunger() {
-	
-}
+	@Test
+	public void shouldPerformTickIncreaseHungerBoredomThirstBy2() {
+		int preTestHunger = testPet.getHunger();
+		int preTestThirst = testPet.getThirst();
+		int preTestBoredom = testPet.getBoredom();
+		testPet.singleTick();
+		int underTestHunger = testPet.getHunger();
+		int underTestThirst = testPet.getThirst();
+		int underTestBoredom = testPet.getThirst();
+		assertTrue(preTestHunger + 2 == underTestHunger && preTestThirst + 2 == underTestThirst
+				&& preTestBoredom + 2 == underTestBoredom);
+	}
 
-@Test
-public void shouldReturnThirst() {
-	
-}
-
-@Test
-public void shouldReturnBoredom() {
-	
-}
-
-@Test
-public void shouldPerformATick() {
-	
-}
-
-}
+	/*
+	 * @Test public void shouldReturnHunger() { testPet.getHunger(); }
+	 * 
+	 * @Test public void shouldReturnThirst() { testPet.getThirst(); }
+	 * 
+	 * @Test public void shouldReturnBoredom() { testPet.getBoredom(); }
+	 */}
